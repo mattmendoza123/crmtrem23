e50c6a73d72253f4e7041b2b850c94588d504811
MattMendoza <mendozamattnoruel@gmail.com>
2023-02-10 15:50:54 +0800

    adminaff

--- application/views/includes/sidebar.php
+++ application/views/includes/sidebar.php
@@ -107,7 +107,7 @@
                         <li> <a class="waves-effect" href="<?= base_url("crm") ?>" aria-expanded="false"><i class="fas fa-copy" style="color: #fff;"></i><span class="hide-menu">CRM</span></a></li> -->
                         <li> <a class="waves-effect" href="<?= base_url("manageuser") ?>" aria-expanded="false"><i class="fas fa-copy" style="color: #fff;"></i><span class="hide-menu">Manage Advertiser and Affiliate</span></a></li>
                         <li> <a class="waves-effect" href="<?= base_url("admincrmads") ?>" aria-expanded="false"><i class="fas fa-copy" style="color: #fff;"></i><span class="hide-menu">CRM Advertiser</span></a></li>
-                        <li> <a class="waves-effect" href="<?= base_url("crmaff") ?>" aria-expanded="false"><i class="fas fa-copy" style="color: #fff;"></i><span class="hide-menu">CRM Affiliate</span></a></li>
+                        <li> <a class="waves-effect" href="<?= base_url("admincrmaff") ?>" aria-expanded="false"><i class="fas fa-copy" style="color: #fff;"></i><span class="hide-menu">CRM Affiliate</span></a></li>
                         <li> <a class="waves-effect" href="<?= base_url("ipwhitelist") ?>" aria-expanded="false"><i class="fas fa-copy" style="color: #fff;"></i><span class="hide-menu">IP Whitelist</span></a></li>
                     </li>
                 </ul>
