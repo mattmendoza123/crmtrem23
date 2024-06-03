d249982eb1ab9325b7f08a3cc5c36fc3fd336d30
iamjerson <iamjerson@gmail.com>
2024-06-03 18:25:48 -0400

    Update admincrmads_footer.php

--- application/modules/admincrmads/views/admincrmads_footer.php
+++ application/modules/admincrmads/views/admincrmads_footer.php
@@ -2,7 +2,7 @@
 <!-- <script src="<?=base_url()?>assets/module/bootstrap/js/bootstrap.min.js"></script> -->
 <script type="text/javascript" src="https://unpkg.com/xlsx@0.15.1/dist/xlsx.full.min.js"></script>
 <script src="https://cdn.datatables.net/v/dt/dt-2.0.8/datatables.min.js"></script>
-<script src="<?= base_url() . "assets"; ?>/js/responsive.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/3.0.2/js/dataTables.responsive.min.js"></script>
 <script src="<?= base_url() . "assets"; ?>/js/calcheight.min.js"></script>
 <script src="<?= base_url() . "assets"; ?>/js/table2csv.js"></script>
 <script src="<?= base_url() . "assets"; ?>/js/multiselect-dropdown.js"></script>
