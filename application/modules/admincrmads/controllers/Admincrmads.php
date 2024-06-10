6caa2bc714f7b412aa43856c7e4cf32519f3606c
iamjerson <iamjerson@gmail.com>
2024-06-10 13:59:24 -0400

    Update Admincrmads.php

--- application/modules/admincrmads/controllers/Admincrmads.php
+++ application/modules/admincrmads/controllers/Admincrmads.php
@@ -185,7 +185,7 @@ class Admincrmads extends MY_Controller
 			"recordsFiltered" => $crmads->num_rows(),
 			"data" => $data, 
 			"ads_tags"=> $this->makeOptions("ads_tags",$crmads->result() ),		
-			"dataxxx"=> $_POST['from_date'],	
+			"dataxxx"=> $_POST,	
 		);
 		echo json_encode($output);
 		exit();
