584484e4108cec597b435bf41810f2811b17dd89
iamjerson <iamjerson@gmail.com>
2024-06-17 15:27:25 -0400

    Update Newoffers.php

--- application/modules/newoffers/controllers/Newoffers.php
+++ application/modules/newoffers/controllers/Newoffers.php
@@ -39,8 +39,9 @@ class Newoffers extends MY_Controller
 	}
 	$data = json_decode(file_get_contents($url)); // Make the request and get the response
 	//echo $data; // Return the response to your frontend code
-    
-	foreach (json_decode($data['info']->offers) as $tm) {
+    print_r($data);
+	die;
+	foreach ($data['info']->offers as $tm) {
 			
 			$data[] = array(
 				// $tm->crm_details_id,
