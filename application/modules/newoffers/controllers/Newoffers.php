8dce5a1c6295337d6d521fa1aa1bec90a471e127
iamjerson <iamjerson@gmail.com>
2024-06-17 15:51:39 -0400

    Update Newoffers.php

--- application/modules/newoffers/controllers/Newoffers.php
+++ application/modules/newoffers/controllers/Newoffers.php
@@ -33,6 +33,18 @@ class Newoffers extends MY_Controller
 	$order = $this->input->post("order");
 	$search = $this->input->post("search");
 
+	$col = 0;
+	$dir = "";
+	if (!empty($order)) {
+		foreach ($order as $o) {
+			$col = $o['column'];
+			$dir = $o['dir'];
+		}
+	}
+
+	if ($dir != "asc" && $dir != "desc") {
+		$dir = "desc";
+	}
 
 	header('Access-Control-Allow-Origin: *'); // Allow requests from any domain
 	header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
@@ -61,8 +73,8 @@ class Newoffers extends MY_Controller
 
 		$output = array(
 			"draw" => $draw,
-			"recordsTotal" => 0,
-			"recordsFiltered" => 0,
+			"recordsTotal" => count($data_arr),
+			"recordsFiltered" => count($data_arr),
 			"data" => $data_arr
 		);
 		echo json_encode($output);
