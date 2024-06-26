6213059cb76d27efe16efd9858f64f2578021193
iamjerson <iamjerson@gmail.com>
2024-06-26 17:54:52 -0400

    updates

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,11 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-
-class Api extends MY_Controller
+class Api extends CI_Controller
 {
-
-	private $errmsg = "";
-
 	public function __construct()
 	{
 		parent::__construct();
@@ -22,20 +18,14 @@ class Api extends MY_Controller
 		header("Access-Control-Allow-Headers: Content-Type, Authorization");
 		
 		$start_date = $this->input->get("rangeFrom") !="" ? $this->input->get("rangeFrom") : "2023-01-01";		
-		$end_date = date("Y-m-d");	
-		
-		$continue = true;
+		$end_date = date("Y-m-d");			
 		$page = $this->input->get('page', TRUE);
 		$perPage = $this->input->get('perPage', TRUE);
 
 		$transaction_data = [];
 		$data_headers = [];
-	
-		$url = 'https://tremendio.scaletrk.com/api/v2/network/reports/conversions?api-key=aafcf12b64ca3230279a89aa8b6eacf03c7c59da&lang=en&sortField=added_timestamp&sortDirection=desc&perPage='.$perPage.'&page='.$page.'&rangeFrom='.$start_date.'&rangeTo='.$end_date.'&columns=sub_id1,sub_id2,sub_id3,revenue,added_timestamp,changed_timestamp,currency,transaction_id,advertiser,affiliate&filters=advertisers:500,affiliates:1602'; // URL of the API you want to request
-		
-		$data = json_decode(file_get_contents($url), true); 		
-
-		
+		$url = 'https://tremendio.scaletrk.com/api/v2/network/reports/conversions?api-key=aafcf12b64ca3230279a89aa8b6eacf03c7c59da&lang=en&sortField=added_timestamp&sortDirection=desc&perPage='.$perPage.'&page='.$page.'&rangeFrom='.$start_date.'&rangeTo='.$end_date.'&columns=sub_id1,sub_id2,sub_id3,revenue,added_timestamp,changed_timestamp,currency,transaction_id,advertiser,affiliate&filters=advertisers:500,affiliates:1602'; // URL of the API you want to request	
+		$data = json_decode(file_get_contents($url), true); 			
 		foreach($data['info']['transactions'] as $transaction){						
 			unset($transaction["affiliate"]["value"] , $transaction["advertiser"]["value"]);	
 			$transaction_data[] = $transaction;		
