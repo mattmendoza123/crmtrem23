5a4f314bd8ea4b8565179f6011f982a8ab9268ef
iamjerson <iamjerson@gmail.com>
2024-06-26 17:45:08 -0400

    Update Api.php

--- application/modules/api/controllers/Api.php
+++ application/modules/api/controllers/Api.php
@@ -34,11 +34,11 @@ class Api extends MY_Controller
 		$transaction_data = [];
 		$data_headers = [];
 	
-		$url = 'https://tremendio.scaletrk.com/api/v2/network/reports/conversions?api-key=aafcf12b64ca3230279a89aa8b6eacf03c7c59da&lang=en&sortField=added_timestamp&sortDirection=desc&perPage='.$perPage.'&page='.$page.'&rangeFrom="'.$start_date.'"&rangeTo="'.$end_date.'"&columns=sub_id1,sub_id2,sub_id3,revenue,added_timestamp,changed_timestamp,currency,transaction_id,advertiser,affiliate&filters=advertisers:500,affiliates:1602'; // URL of the API you want to request
-		echo $url;
+		$url = 'https://tremendio.scaletrk.com/api/v2/network/reports/conversions?api-key=aafcf12b64ca3230279a89aa8b6eacf03c7c59da&lang=en&sortField=added_timestamp&sortDirection=desc&perPage='.$perPage.'&page='.$page.'&rangeFrom='.$start_date.'&rangeTo='.$end_date.'&columns=sub_id1,sub_id2,sub_id3,revenue,added_timestamp,changed_timestamp,currency,transaction_id,advertiser,affiliate&filters=advertisers:500,affiliates:1602'; // URL of the API you want to request
+		
 		$data = json_decode(file_get_contents($url), true); 		
 
-		print_r($data);
+		
 		foreach($data['info']['transactions'] as $transaction){						
 			unset($transaction["affiliate"]["value"] , $transaction["advertiser"]["value"]);	
 			$transaction_data[] = $transaction;		
