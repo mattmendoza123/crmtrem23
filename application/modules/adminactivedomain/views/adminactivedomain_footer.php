20ba2bfcb43ac3bb699f3ad154be526e283e62bb
mattmendoza123 <mendozamattnoruel@gmail.com>
2024-08-07 18:26:14 +0800

    update

--- application/modules/adminactivedomain/views/adminactivedomain_footer.php
+++ application/modules/adminactivedomain/views/adminactivedomain_footer.php
@@ -65,7 +65,7 @@ $(document).ready(function () {
     var base_url = "https://crm.tremendio.network/";
 
     var dataTable = $('#activedomain').DataTable({
-        "pageLength": 5,
+        "pageLength": 10,
         "order": [[3, "desc"]]
     });
 
