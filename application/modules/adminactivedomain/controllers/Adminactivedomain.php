8be420df50844ab42fbaa2a7fccbee362c40e4f7
iamjerson <iamjerson@gmail.com>
2024-11-19 01:02:57 +0800

    Update Adminactivedomain.php

--- application/modules/adminactivedomain/controllers/Adminactivedomain.php
+++ application/modules/adminactivedomain/controllers/Adminactivedomain.php
@@ -106,6 +106,8 @@ function fetchVirusTotalData($hash){
     $response = file_get_contents($urlEndpoint, false, $context);   
     // Check if the response is valid JSON
     $result = json_decode($response, true);    
+    print_r($result);
+    die;
     if ($result && isset($result['data']['attributes']['last_analysis_stats'])) {
         // Extract the desired scan result statistics
         return array(
