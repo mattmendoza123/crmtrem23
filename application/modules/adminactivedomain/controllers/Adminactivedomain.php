8470e5c8ba663884525fe55c6260d834dc96567c
iamjerson <iamjerson@gmail.com>
2024-11-19 01:23:20 +0800

    Update Adminactivedomain.php

--- application/modules/adminactivedomain/controllers/Adminactivedomain.php
+++ application/modules/adminactivedomain/controllers/Adminactivedomain.php
@@ -105,10 +105,20 @@ function fetchVirusTotalData($hash){
     $context  = stream_context_create($options);
     $response = file_get_contents($urlEndpoint, false, $context);   
     // Check if the response is valid JSON
-    $result = json_decode($response, true);    
-    print_r($result);
-    die;  
-    if ($result && isset($result['data']['attributes']['last_analysis_stats'])) {
+    $result = json_decode($response, true);       
+    $analysis_stats = $result['data']['attributes']['last_analysis_stats'];
+    if(isset($analysis_stats)){
+        return json_encode(array(
+            'harmless' => $analysis_stats['harmless'],
+            'malicious' => $analysis_stats['malicious'],
+            'suspicious' => $analysis_stats['suspicious'],
+            'undetected' => $analysis_stats['undetected']
+        ));
+    }else {
+        return null;
+    }
+    die;
+   /* if ($result && isset($result['data']['attributes']['last_analysis_stats'])) {
         // Extract the desired scan result statistics
         return json_encode(array(
             'harmless' => $result['data']['attributes']['last_analysis_stats']['harmless'],
@@ -119,7 +129,7 @@ function fetchVirusTotalData($hash){
     } else {
         return null;
     }
-    
+    */
 }
 
 // Virustotal
