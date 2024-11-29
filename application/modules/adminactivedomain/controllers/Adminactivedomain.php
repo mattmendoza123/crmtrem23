3baa83282ea8b052bcedaabfc6b51f9a03de3cd4
iamjerson <iamjerson@gmail.com>
2024-11-29 21:47:16 +0800

    Update Adminactivedomain.php

--- application/modules/adminactivedomain/controllers/Adminactivedomain.php
+++ application/modules/adminactivedomain/controllers/Adminactivedomain.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-date_default_timezone_set('Asia/Manila'); 
+date_default_timezone_set('America/New_York'); 
 class Adminactivedomain extends MY_Controller
 {
 
