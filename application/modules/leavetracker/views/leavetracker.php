83285caf997745f8284d04962ada7ecfaf9e3e4d
mattmendoza123 <mendozamattnoruel@gmail.com>
2025-04-03 20:02:55 +0800

    leave tracker

---
+++ application/modules/leavetracker/views/leavetracker.php
@@ -0,0 +1,51 @@
+<div class="page-wrapper">
+    <div class="container-fluid">
+        <div class="row page-titles">
+            <div class="col-md-12 align-self-center">
+                <h3 class="text-themecolor">Leave Tracker</h3>
+            </div>
+        </div>
+
+        <style>
+        /* body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0;
+            background-color: #f4f4f4;
+        } */
+        .form-container {
+            width: 640px;
+            height: 1700px;
+            background: white;
+            padding: 20px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            border-radius: 10px;
+            overflow: hidden;
+        }
+        iframe {
+        width: 100%;
+        height: 100%;
+        border: none;
+        overflow: hidden;
+        scrollbar-width: none; /* Firefox */
+        -ms-overflow-style: none; /* Internet Explorer/Edge */
+        }
+
+        iframe::-webkit-scrollbar {
+            display: none; /* Chrome, Safari */
+        }
+    </style>
+</head>
+<body>
+    <div class="form-container">
+    <iframe 
+        src="https://docs.google.com/spreadsheets/d/1r4w4ciJe3NymR2Gfkvv_1jPuH7wiDd28e1AQi9zXQRY/preview?gid=824835850" 
+        allowfullscreen>
+    </iframe>
+    </div>
+    </div>
+</div>
+    
+
+
