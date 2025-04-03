a7aabf23b24695f23583f362fcdbf4657d5eddc2
mattmendoza123 <mendozamattnoruel@gmail.com>
2025-04-03 19:06:06 +0800

    update

--- application/modules/leaverequest/views/leaverequest.php
+++ application/modules/leaverequest/views/leaverequest.php
@@ -1,31 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Request Leave</title>
-    <style>
-        body {
+<div class="page-wrapper">
+    <div class="container-fluid">
+        <div class="row page-titles">
+            <div class="col-md-12 align-self-center">
+                <h3 class="text-themecolor">Request Leave</h3>
+            </div>
+        </div>
+
+        <style>
+        /* body {
             display: flex;
             justify-content: center;
             align-items: center;
-            min-height: 100vh;
             margin: 0;
             background-color: #f4f4f4;
-        }
-        .form-container {
+        } */
+        /* .form-container {
             width: 640px;
-            height: 900px; /* Adjusted height */
+            height: 1700px;
             background: white;
             padding: 20px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
             border-radius: 10px;
             overflow: hidden;
-        }
+        } */
         iframe {
-            width: 100%;
-            height: 100%;
-            border: none;
+        width: 100%;
+        height: 100%;
+        border: none;
+        overflow: hidden;
+        scrollbar-width: none; /* Firefox */
+        -ms-overflow-style: none; /* Internet Explorer/Edge */
+        }
+
+        iframe::-webkit-scrollbar {
+            display: none; /* Chrome, Safari */
         }
     </style>
 </head>
@@ -33,5 +41,8 @@
     <div class="form-container">
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSds4Bc8tFosbcPZJASTZ_H0lNOMCEBkhjHdgVRq0XDJB9hh_w/viewform?embedded=true"></iframe>
     </div>
-</body>
-</html>
+    </div>
+</div>
+    
+
+
